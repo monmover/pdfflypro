69a950fc9452da17ff49a206ea5d3012d172288f
monmover <35196143+monmover@users.noreply.github.com>
2026-01-31 22:56:58 -0300

    Update merge.js

--- api/merge.js
+++ api/merge.js
@@ -1,1 +1,21 @@
+import { PDFDocument } from "pdf-lib";
+import multer from "multer";
+
+const upload = multer();
+
+export default async function handler(req, res) {
+  upload.array("files")(req, {}, async () => {
+    const pdfFinal = await PDFDocument.create();
+
+    for (const file of req.files) {
+      const pdf = await PDFDocument.load(file.buffer);
+      const pages = await pdfFinal.copyPages(pdf, pdf.getPageIndices());
+      pages.forEach(p => pdfFinal.addPage(p));
+    }
+
+    const bytes = await pdfFinal.save();
+    res.setHeader("Content-Type", "application/pdf");
+    res.send(Buffer.from(bytes));
+  });
+}
 
